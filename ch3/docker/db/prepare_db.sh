98a81c372ff6da90ada1a4d69894260ff3241730
Jaime Buelta <jaime.buelta@gmail.com>
2019-06-02 12:54:12 +0100

    Add the server to the docker-compose

--- ch3/docker/db/prepare_db.sh
+++ ch3/docker/db/prepare_db.sh
@@ -6,7 +6,4 @@ set -e
 echo 'Creating Schema'
 python3 /opt/code/init_db.py
 
-echo 'Loading initial data'
-python3 /opt/code/load_from_csv.py -f /opt/code/db/input.txt
-
 /opt/code/db/stop_postgres.sh
