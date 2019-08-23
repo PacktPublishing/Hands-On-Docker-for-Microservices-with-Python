de10595e3b96caf72322def017e14e913847da1d
Jaime Buelta <jaime.buelta@gmail.com>
2019-08-23 19:55:18 +0100

    Tweak build scripts

--- ch11/microservices/thoughts_backend/build-ci.sh
+++ ch11/microservices/thoughts_backend/build-ci.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+if [ -z "$1" ]
+  then
+    # Error, not version name
+    echo "No VERSION_NAME supplied"
+    exit -1
+fi
+
 VERSION_SHA=`git log --format=format:%H -n 1`
-VERSION_NAME=`git describe --tags`
+VERSION_NAME=$1
+
+echo "Building version \"${VERSION_NAME}\""
+
 docker-compose build --build-arg VERSION_NAME=${VERSION_NAME} --build-arg VERSION_SHA=${VERSION_SHA}
 
 # tag the resulting image with the version
